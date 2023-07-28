30218e369fe918284a2d62b57c2adb8ac8e52573
M Zakiy Nurhasyim <zakiynurhasyim@datindo.co.id>
2023-07-28 11:24:15 +0700

    add problem solving 1-3

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,6 @@
 <template>
     <section id="container">
-      <div style="display: flex; flex-direction: row; justify-content: end; color: aliceblue; width: 73%;">
+      <div style="display: flex; flex-direction: row; justify-content: end; color: aliceblue; width: 72%;">
         <h5>
           {{ weather }}
         </h5>
@@ -9,6 +9,16 @@
       <div class="tab">
         <button type="button" class="btn btn-sm btn-primary" @click="pushToList">List</button>
         <button type="button" class="btn btn-sm btn-primary" @click="pushToAddTodo">Add New Todo</button>
+      </div>
+      <div style="display: flex; flex-direction: row; justify-content: end; color: aliceblue; width: 72%;">
+        <div class="input-group rounded" style="display: flex; flex-direction: row; justify-content: end; width: 25%;">
+          <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+          <span class="input-group-text border-0" id="search-addon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg>
+          </span>
+        </div>
       </div>
       <router-view />
     </section>
